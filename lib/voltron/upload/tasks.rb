e92cf07bc080dc8bad1dd04abe60e178dfa2572d
Eric Hainer <eric.hainer@gmail.com>
2016-10-23 15:52:32 -0600

    Adds tasks spec

--- lib/voltron/upload/tasks.rb
+++ lib/voltron/upload/tasks.rb
@@ -3,7 +3,7 @@ module Voltron
     class Tasks
 
       def self.cleanup
-        Voltron::Temp.where("created_at <= ?", Voltron.config.upload.keep_for.ago).delete_all
+        Voltron::Temp.where("created_at <= ?", Voltron.config.upload.keep_for.ago).destroy_all
       end
 
     end
