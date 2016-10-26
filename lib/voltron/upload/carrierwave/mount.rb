cff91b8da09f5afcc9de46fc73c8f29e28d0cef0
Eric Hainer <eric.hainer@gmail.com>
2016-10-26 14:10:35 -0600

    Removes class_eval

--- lib/voltron/upload/carrierwave/mount.rb
+++ lib/voltron/upload/carrierwave/mount.rb
@@ -8,11 +8,7 @@ module Voltron
 
           column = args.first.to_sym
 
-          class_eval do
-
-            attr_accessor "commit_#{column}".to_sym
-
-          end
+          attr_accessor "commit_#{column}".to_sym
         end
 
         def mount_uploaders(*args)
@@ -20,32 +16,29 @@ module Voltron
 
           column = args.first.to_sym
 
-          class_eval do
+          attr_accessor "commit_#{column}".to_sym
 
-            attr_accessor "commit_#{column}".to_sym
+          attr_accessor "remove_#{column}".to_sym
 
-            attr_accessor "remove_#{column}".to_sym
+          before_validation do
+            # Merge any new uploads with the pre-existing uploads
+            uploads = Array.wrap(self.send("#{column}_was")) | Array.wrap(self.send(column))
 
-            before_validation do
-              # Merge any new uploads with the pre-existing uploads
-              uploads = Array.wrap(self.send("#{column}_was")) | Array.wrap(self.send(column))
+            assign_attributes(column => uploads)
+          end
 
-              assign_attributes(column => uploads)
-            end
+          before_validation do
+            # Get the filenames of uploads we want to remove
+            remove = Array.wrap(self.send("remove_#{column}"))
 
-            before_validation do
-              # Get the filenames of uploads we want to remove
-              remove = Array.wrap(self.send("remove_#{column}"))
+            # Get the current uploads
+            uploads = Array.wrap(self.send(column))
 
-              # Get the current uploads
-              uploads = Array.wrap(self.send(column))
+            # Go through each file to remove, attempt to find it, and remove it.
+            # Only remove a unique file name as many times as it exists in the remove array
+            remove.each { |r| uploads.find { |u| u.file.try(:filename) == r }.try(:remove!) }
 
-              # Go through each file to remove, attempt to find it, and remove it.
-              # Only remove a unique file name as many times as it exists in the remove array
-              remove.each { |r| uploads.find { |u| u.file.try(:filename) == r }.try(:remove!) }
-
-              assign_attributes(column => uploads.compact)
-            end
+            assign_attributes(column => uploads.compact)
           end
         end
 
