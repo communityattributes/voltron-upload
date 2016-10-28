98cbc0279f03373235cff0843f9db34c60657874
Eric Hainer <eric.hainer@gmail.com>
2016-10-27 21:24:44 -0600

    Revises spec to avoid integrity constraint violation

--- spec/models/voltron/temp_spec.rb
+++ spec/models/voltron/temp_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 describe Voltron::Temp do
 
   before(:each) do
+    Voltron::Temp.delete_all
     Voltron::Temp.create(uuid: "abc", column: "avatar", file: File.open(File.expand_path("../../../fixtures/files/1.jpg", __FILE__)), multiple: false)
     Voltron::Temp.create(uuid: "def", column: "images", file: File.open(File.expand_path("../../../fixtures/files/2.jpg", __FILE__)), multiple: true)
     Voltron::Temp.create(uuid: "ghi", column: "images", file: File.open(File.expand_path("../../../fixtures/files/3.jpg", __FILE__)), multiple: true)
