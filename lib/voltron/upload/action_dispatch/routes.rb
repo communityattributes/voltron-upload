ca68d050a1b9e24732de17fb0bf907fb38da9e5d
Eric Hainer <eric.hainer@gmail.com>
2017-04-10 13:11:52 -0600

    [CHG] upload_for now accepts options for path and controller, but can now only accept one resource at a time

--- lib/voltron/upload/action_dispatch/routes.rb
+++ lib/voltron/upload/action_dispatch/routes.rb
@@ -2,11 +2,14 @@ module Voltron
   module Upload
     module Routes
 
-      def upload_for(*resources)
-        resources.each do |resource|
-          controller = resource.to_s.underscore
-          post "/#{controller}/upload", to: "#{controller}#upload", as: "upload_#{controller}"
-        end
+      def upload_for(resource, options={})
+        controller = resource.to_s.underscore
+        options = options.with_indifferent_access
+
+        options[:path] ||= "/#{controller}/upload"
+        options[:controller] ||= controller
+
+        post options[:path].gsub(/(^\/+|\/+$)/, ''), to: "#{options[:controller]}#upload", as: "upload_#{controller}"
       end
 
     end
